de0048b7ba0c6dcab7ca860145e0ad65ed1d8d42
Zichun (Jerry) Gao <zichungao88@gmail.com>
2024-11-01 23:09:30 -0500

    Setup Project 2 Workspace

---
+++ project2.py
@@ -0,0 +1,38 @@
+import numpy as np
+import struct
+from array import array
+from os.path import join
+import random
+import matplotlib.pyplot as plt
+
+# 1 TODO: Download & load MNIST training & testing datsets
+
+
+
+
+# 2 TODO: Identify row & column indices of pixels with nonzero intensities
+
+
+
+
+# 3 TODO: Construct matrix A & vector y, solve the least square, & plot values of entries of theta
+
+
+
+
+# 4 TODO: Load images & calculate error rate, false positive rate, & false negative rate of classifier
+
+
+
+
+# 5 TODO: Repeat steps 1-4 w/ only the 1st 100 images
+
+
+
+
+# 6 TODO: Changing the feature set
+
+
+
+
+# 7 TODO: Calculate error rate, false positive, & false negative rate for the new classifier
