55407286b2bc4c97b1e92eda68b800f067ab2aef
Zichun (Jerry) Gao <zichungao88@gmail.com>
2024-11-03 18:29:52 -0600

    Load necessary ubyte data

--- project2.py
+++ project2.py
@@ -4,10 +4,51 @@ from array import array
 from os.path import join
 import random
 import matplotlib.pyplot as plt
+import mnist_data_loader # class from mnist_data_loader.py
 
 # 1 TODO: Download & load MNIST training & testing datsets
 
+# Set file paths
+input_path = './'
+training_images_filepath = join(input_path, 'train-images-idx3-ubyte')
+training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte')
+test_images_filepath = join(input_path, 't10k-images-idx3-ubyte')
+test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte')
 
+# Function to display images
+def show_images(images, title_texts):
+    cols = 5
+    rows = int(len(images) / cols) + 1
+    plt.figure(figsize = (30, 20))
+    index = 1    
+    for i in zip(images, title_texts):        
+        image = i[0]        
+        title_text = i[1]
+        plt.subplot(rows, cols, index)        
+        plt.imshow(image, cmap = plt.cm.gray)
+        if (title_text != ''):
+            plt.title(title_text, fontsize = 15);        
+        index += 1
+    plt.show()
+
+# Load given MNIST dataset
+mnist_dataloader = mnist_data_loader.mnist_data_loader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
+(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+
+# Display loaded images
+images_2_show = []
+titles_2_show = []
+for i in range(10):
+    r = random.randint(1, 5000)
+    images_2_show.append(x_train[r])
+    titles_2_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))    
+
+for i in range(5):
+    r = random.randint(1, 5000)
+    images_2_show.append(x_test[r])        
+    titles_2_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))    
+
+# show_images(images_2_show, titles_2_show)
 
 
 # 2 TODO: Identify row & column indices of pixels with nonzero intensities
